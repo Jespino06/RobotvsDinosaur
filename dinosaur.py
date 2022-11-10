8dbf209dba84a11e1769dfeab9233825fde208eb
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-10 02:58:58 -0500

    added methods to Dinosaur

--- dinosaur.py
+++ dinosaur.py
@@ -1,12 +1,15 @@
 class Dinosaur:
     def __init__(self):
-    self.name = ''
-    self.health = 100
-    self.attack_power = 100
+        self.name = ''
+        self.health = 100
+        self.attack_power = 100
+
+    def __init__(self, name, attack_power):
+        self.name = ""
+        self.attack_power = int
+
     
-
-
-
-
-
+    def attack(self, robot):
+        self.name = ''
+        self.robot = True
 
