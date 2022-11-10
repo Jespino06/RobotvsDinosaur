667c0bdb79d9f3526576886845eaec32e1e9e43a
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-10 02:52:54 -0500

    added additional methods for battefield

--- battle_field.py
+++ battle_field.py
@@ -1,11 +1,35 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
 class Battle_field:
     def __init__(self):
         self.attack_active = True
 
-def battle_over():
-    if self.health == 0:
-        print('Game over!')
-    elif self.health > 0:
-        continue
-    
+    def run_game(self):
+
+
+        def display_welcome(self):
+            self.welcome = "Welcome to the battlefield, the game will begin!"
+            print(self.welcome)
+
+        display_welcome()
+
+        def battle_phase(self):
+            if self.phase == self.name:
+                print("Level 1!")
+            elif self.phase >= 2:
+                print("Level 2!")
+            elif self.phase >= 3:
+                print("Level 3!")
+        
+        battle_phase()
+
+        def display_winner(self):
+            if self.health == 0:
+                print('Game over!')
+            elif self.health > 0:
+                print(f"The winner is {self.name} !!!")
+
+        display_winner()
+        
     
