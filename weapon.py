b8b5e240eaba5896cfb9cb608e19c8ba0e2a5241
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-10 03:05:04 -0500

    added methods to weapon file

--- weapon.py
+++ weapon.py
@@ -2,12 +2,17 @@ class Weapon:
     def __init__(self):
     self.name = ''
     self.attack_power = 100
-    self.damage_per_attack = int
+    
+    def __init__(self, name, attack_power):
+        self.name = ""
+        self.attack_power = int
+        self.damage_per_attack = int
 
-def laser_gun(self):
-    self.attack_power = 20
-    self.damage_per_attack = 10
-    self.health -= self.damage_per_attack
+
+    def laser_gun(self):
+        self.attack_power = 20
+        self.damage_per_attack = 10
+        self.health -= self.damage_per_attack
     
 
 
